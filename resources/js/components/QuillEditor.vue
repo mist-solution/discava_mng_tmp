3fe483b18375b1b473a22e9e25de46a01ab12b00
Seikeirinn <hl_sheng@mistnet.co.jp>
2022-01-19 20:09:20 +0900

    リッチテキストエディタ修正

--- resources/js/components/QuillEditor.vue
+++ resources/js/components/QuillEditor.vue
@@ -97,9 +97,7 @@
 <script>
 import { QuillEditor } from "@vueup/vue-quill";
 import "@vueup/vue-quill/dist/vue-quill.snow.css";
-import "quill/dist/quill.core.css";
-import "quill/dist/quill.snow.css";
-import "quill/dist/quill.bubble.css";
+
 export default {
   components: {
     QuillEditor,

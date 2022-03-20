707acbd173d117ded05a014f88781f9bb3284f14
yamaki <yamaki@mistnet.co.jp>
2022-03-20 09:49:28 +0900

    パスワードリセット対応

---
+++ resources/lang/ja/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'パスワードがリセットされました。',
+    'sent' => 'パスワードリセットのリンクを送信しました。',
+    'throttled' => 'Please wait before retrying.',
+    'token' => 'パスワードリセットトークンが無効です。',
+    'user' => "We can't find a user with that email address.",
+
+];

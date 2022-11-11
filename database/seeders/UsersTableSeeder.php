acaa06743dd417fd9d8e7a341bf783026befdf16
taka_saito <taka_saito@mistnet.co.jp>
2022-11-11 17:34:22 +0900

    seedユーザデータの追加

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -31,35 +31,82 @@ class UsersTableSeeder extends Seeder
         ]);
 
         // 社内検証用ユーザ
-        User::create([
-            'customer_id' => '1',
-            'login_user_id' => 'mist_test001',
-            'mail' => 'integration-test+001@mistnet.co.jp',
-            'name' => 'mist_test001',
-            'initial_password' => bcrypt('Passw0rd'),
-            'password' => bcrypt('Passw0rd"'),
-            'login_fail_count' => '0',
-            'email_verified_at' => null,
-            'remember_token' => null,
-            'add_account' => '1',
-            'upd_account' => '1',
-            'del_flg' => '0',
-        ]);
-        
-        User::create([
-            'customer_id' => '1',
-            'login_user_id' => 'mist_test002',
-            'mail' => 'integration-test+002@mistnet.co.jp',
-            'name' => 'mist_test002',
-            'initial_password' => bcrypt('Passw0rd'),
-            'password' => bcrypt('Passw0rd"'),
-            'login_fail_count' => '0',
-            'email_verified_at' => null,
-            'remember_token' => null,
-            'add_account' => '1',
-            'upd_account' => '1',
-            'del_flg' => '0',
-        ]);
+        $array1 = array("mate1111", "mate2222", "mate3333", "mate4444", "mate5555");
+        $array2 = array("mate6666", "mate7777", "mate8888", "mate9999");
+
+        for($i = 0; $i < count($array1); $i++){
+            User::create([
+                'customer_id' => '1',
+                'login_user_id' => $array1[$i],
+                'mail' => 'integration-test+' . $array1[$i] . '@mistnet.co.jp',
+                'name' => 'mist_' . $array1[$i],
+                'initial_password' => bcrypt($array1[$i]),
+                'password' => bcrypt($array1[$i]),
+                'login_fail_count' => '0',
+                'email_verified_at' => null,
+                'remember_token' => null,
+                'add_account' => '1',
+                'upd_account' => '1',
+                'del_flg' => '0',
+            ]);
+        }
+
+        for($i = 0; $i < count($array2); $i++){
+            User::create([
+                'customer_id' => '2',
+                'login_user_id' => $array2[$i],
+                'mail' => 'integration-test+' . $array2[$i] . '@mistnet.co.jp',
+                'name' => 'mist_' . $array2[$i],
+                'initial_password' => bcrypt($array2[$i]),
+                'password' => bcrypt($array2[$i]),
+                'login_fail_count' => '0',
+                'email_verified_at' => null,
+                'remember_token' => null,
+                'add_account' => '1',
+                'upd_account' => '1',
+                'del_flg' => '0',
+            ]);
+        }
+
+        $proper = array(
+            "yamaki@mistnet.co.jp",
+            "iida_t@mistnet.co.jp",
+            "takahashi_sy@mistnet.co.jp",
+            "hl_sheng@mistnet.co.jp",
+            "saruwatari_e@mistnet.co.jp",
+            "t_tanaka@mistnet.co.jp",
+            "takeuchi@mistnet.co.jp",
+            "nonomiya@mistnet.co.jp",
+            "ny_hashimoto@mistnet.co.jp",
+            "yano_a@discava.net",
+            "hiroki_abe@mistnet.co.jp",
+            "m_yasukawa@mistnet.co.jp",
+            "r_yoshino@mistnet.co.jp",
+            "gaku_ikegami@mistnet.co.jp",
+            "dai_toyoda@mistnet.co.jp",
+            "iimura@mistnet.co.jp",
+            "takao_iida@mistnet.co.jp",
+            "kawasaki_t@mistnet.co.jp",
+        );
+
+        for($i = 0; $i < count($proper); $i++){
+            $pos = strpos($proper[$i], '@') + 1;
+            $userid = substr($proper[$i], 0, $pos);
+            User::create([
+                'customer_id' => '1',
+                'login_user_id' => $proper[$i],
+                'mail' => 'integration-test+' . $proper[$i] . '@mistnet.co.jp',
+                'name' => 'mist_' . $proper[$i],
+                'initial_password' => bcrypt($proper[$i]),
+                'password' => bcrypt($proper[$i]),
+                'login_fail_count' => '0',
+                'email_verified_at' => null,
+                'remember_token' => null,
+                'add_account' => '1',
+                'upd_account' => '1',
+                'del_flg' => '0',
+            ]);
+        }
 
     }
 }

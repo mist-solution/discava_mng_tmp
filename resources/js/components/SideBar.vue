7b23debd996343f3b31e54d8a93b47078787c114
taka_saito <taka_saito@mistnet.co.jp>
2022-11-15 17:33:49 +0900

    グループメニューサンプル

--- resources/js/components/SideBar.vue
+++ resources/js/components/SideBar.vue
@@ -2,13 +2,16 @@
   <v-navigation-drawer temporary v-model='this.$store.state.sidebar.open'>
     <v-list v-for="item in items" :key="item.id" nav dense>
       <div v-if="item.group">
-        Now you see me
-        <v-list-item link v-bind:to="{ name: item.linkTo }" v-bind:disabled="item.disabled">
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>TRUE:{{ item.title }}</v-list-item-title>
-        </v-list-item>
+        <v-list v-for="submenu in  item.submenus" :key="submenu.id" nav dense>
+          <v-list-group v-bind:key="submenu.title">
+            <v-list-item link v-bind:to="{ name: submenu.linkTo }" v-bind:disabled="submenu.disabled">
+              <v-list-item-icon>
+                <v-icon>{{ submenu.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>TRUE:{{ submenu.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list-group>
+        </v-list>
       </div>
       <div v-else>
         <v-list-item link v-bind:to="{ name: item.linkTo }" v-bind:disabled="item.disabled">
@@ -31,7 +34,7 @@ export default {
         { id: 2, title: "ギャラリー管理", icon: "mdi-folder", linkTo: "",             disabled: true, group: false, },
         { id: 3, title: "アクセス情報",   icon: "mdi-folder", linkTo: "",             disabled: true, group: false, },
         { id: 4, title: "アカウント管理", icon: "mdi-folder", linkTo: "",             disabled: false, group: true, 
-          submenu: [
+          submenus: [
             { id: 401, title: "アカウント一覧", icon: "mdi-folder", linkTo: "enduser.list", disabled: false, },
             { id: 402, title: "アカウント登録", icon: "mdi-folder", linkTo: "enduser.register", disabled: false, },
           ],

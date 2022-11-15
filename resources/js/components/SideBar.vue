578e765f0f839907ff95c897345b7a606f6ae49d
taka_saito <taka_saito@mistnet.co.jp>
2022-11-15 18:45:36 +0900

    グループメニューサンプル

--- resources/js/components/SideBar.vue
+++ resources/js/components/SideBar.vue
@@ -3,10 +3,12 @@
     <v-list v-for="item in items" :key="item.id" nav dense>
       <div v-if="item.group">
         <v-list-group value="true">
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
+          <template v-slot:activator>
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </template>
           <v-list-item v-for="submenu in item.submenus" v-bind:key="submenu.id" link v-bind:to="{ name: submenu.linkTo }" v-bind:disabled="submenu.disabled">
             <v-list-item-icon>
               <v-icon>{{ submenu.icon }}</v-icon>

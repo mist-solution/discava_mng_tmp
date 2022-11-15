2cd17c6befcde8ad915048a32560b15700a544f7
taka_saito <taka_saito@mistnet.co.jp>
2022-11-15 13:55:43 +0900

    メニュー表示

--- resources/js/components/SideBar.vue
+++ resources/js/components/SideBar.vue
@@ -1,7 +1,7 @@
 <template>
   <v-navigation-drawer temporary v-model='this.$store.state.sidebar.open'>
     <v-list v-for="item in items" :key="item.id" nav dense>
-      <v-list-item link v-bind:to="{ name: item.linkTo }" {{ item.disabled }}>
+      <v-list-item link v-bind:to="{ name: item.linkTo }" :disabled="item.disabled">
         <v-list-item-icon>
           <v-icon>{{ item.icon }}</v-icon>
         </v-list-item-icon>
@@ -16,13 +16,13 @@ export default {
     return {
       drawer: true,
       items: [
-        { id: 1,  title: "投稿管理",        icon: "mdi-folder",  linkTo: "news.list",   disabled: "", },
-        { id: 2,  title: "ギャラリー管理",  icon: "mdi-folder",  linkTo: "",             disabled: "disabled", },
-        { id: 3,  title: "アクセス情報",    icon: "mdi-folder",  linkTo: "",             disabled: "disabled", },
-        { id: 4,  title: "アカウント管理",  icon: "mdi-folder",  linkTo: "",             disabled: "", },
-        { id: 5,  title: "契約情報",        icon: "mdi-folder",  linkTo: "",             disabled: "disabled", },
-        { id: 6,  title: "サイト更新依頼",  icon: "mdi-folder",  linkTo: "",             disabled: "disabled", },
-        { id: 7,  title: "WEBサイトへ",     icon: "mdi-folder",  linkTo: "",             disabled: "disabled", },
+        { id: 1,  title: "投稿管理",        icon: "mdi-folder",  linkTo: "news.list",   disabled: "false", },
+        { id: 2,  title: "ギャラリー管理",  icon: "mdi-folder",  linkTo: "",             disabled: "true", },
+        { id: 3,  title: "アクセス情報",    icon: "mdi-folder",  linkTo: "",             disabled: "true", },
+        { id: 4,  title: "アカウント管理",  icon: "mdi-folder",  linkTo: "",             disabled: "false", },
+        { id: 5,  title: "契約情報",        icon: "mdi-folder",  linkTo: "",             disabled: "true", },
+        { id: 6,  title: "サイト更新依頼",  icon: "mdi-folder",  linkTo: "",             disabled: "true", },
+        { id: 7,  title: "WEBサイトへ",     icon: "mdi-folder",  linkTo: "",             disabled: "true", },
       ],
       right: null,
     };

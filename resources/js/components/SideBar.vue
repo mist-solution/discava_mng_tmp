92c08cd0ff928b4bb1c950dd794b9a7abc1cde3b
taka_saito <taka_saito@mistnet.co.jp>
2022-11-15 15:11:18 +0900

    グループメニューサンプル

--- resources/js/components/SideBar.vue
+++ resources/js/components/SideBar.vue
@@ -1,12 +1,23 @@
 <template>
   <v-navigation-drawer temporary v-model='this.$store.state.sidebar.open'>
     <v-list v-for="item in items" :key="item.id" nav dense>
-      <v-list-item link v-bind:to="{ name: item.linkTo }" v-bind:disabled="item.disabled">
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>{{ item.title }}</v-list-item-title>
-      </v-list-item>
+      <div v-if="{ item:group } === true">
+        Now you see me
+        <v-list-item link v-bind:to="{ name: item.linkTo }" v-bind:disabled="item.disabled">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>TRUE:{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </div>
+      <div v-else>
+        <v-list-item link v-bind:to="{ name: item.linkTo }" v-bind:disabled="item.disabled">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </div>
     </v-list>
   </v-navigation-drawer>
 </template>
@@ -16,20 +27,21 @@ export default {
     return {
       drawer: true,
       items: [
-        { id: 1,  title: "投稿管理",        icon: "mdi-folder",  linkTo: "news.list",   disabled: false, },
-        { id: 2,  title: "ギャラリー管理",  icon: "mdi-folder",  linkTo: "",             disabled: true, },
-        { id: 3,  title: "アクセス情報",    icon: "mdi-folder",  linkTo: "",             disabled: true, },
-        { id: 4,  title: "アカウント管理",  icon: "mdi-folder",  linkTo: "enduser.list",             disabled: false, },
-        { id: 5,  title: "契約情報",        icon: "mdi-folder",  linkTo: "",             disabled: true, },
-        { id: 6,  title: "サイト更新依頼",  icon: "mdi-folder",  linkTo: "",             disabled: true, },
-        { id: 7,  title: "WEBサイトへ",     icon: "mdi-folder",  linkTo: "",             disabled: true, },
+        { id: 1, title: "投稿管理",       icon: "mdi-folder", linkTo: "news.list",   disabled: false, group: false, },
+        { id: 2, title: "ギャラリー管理", icon: "mdi-folder", linkTo: "",             disabled: true, group: false, },
+        { id: 3, title: "アクセス情報",   icon: "mdi-folder", linkTo: "",             disabled: true, group: false, },
+        { id: 4, title: "アカウント管理", icon: "mdi-folder", linkTo: "",             disabled: false, group: true, 
+          submenu: [
+            { id: 401, title: "アカウント一覧", icon: "mdi-folder", linkTo: "enduser.list", disabled: "", },
+            { id: 402, title: "アカウント一覧", icon: "mdi-folder", linkTo: "enduser.register", disabled: "", },
+          ],
+        },
+        { id: 5, title: "契約情報",       icon: "mdi-folder", linkTo: "",             disabled: true, group: false, },
+        { id: 6, title: "サイト更新依頼", icon: "mdi-folder", linkTo: "",             disabled: true, group: false, },
+        { id: 7, title: "WEBサイトへ",    icon: "mdi-folder", linkTo: "",             disabled: true, group: false, },
       ],
       right: null,
     };
   },
 };
-//          submenu: [
-//          { id: 401,  title: "アカウント一覧",  icon: "mdi-folder",  linkTo: "enduser.list", disabled: "", },
-//          { id: 402,  title: "アカウント一覧",  icon: "mdi-folder",  linkTo: "enduser.register", disabled: "", },
-//          ] },
 </script>

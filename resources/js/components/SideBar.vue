a5e86f03fd3cc3912b199c77920965a95ed98c27
taka_saito <taka_saito@mistnet.co.jp>
2022-11-16 16:43:13 +0900

    サブメニューのアイコン削除

--- resources/js/components/SideBar.vue
+++ resources/js/components/SideBar.vue
@@ -19,7 +19,6 @@
             v-for="submenu in item.submenus"
             :key="submenu.id"
             :title="submenu.title"
-            :prepend-icon="submenu.icon"
             link
             :to="{ name: submenu.linkTo }"
             :disabled="submenu.disabled"
@@ -40,38 +39,6 @@
 
       </div>
     </v-list>
-<!--
-    <v-list v-for="item in items" :key="item.id" nav dense>
-      <div v-if="item.group">
-        <v-list-group :value="true">
-          <template v-slot:activator>
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </template>
-          <v-list-item v-for="submenu in item.submenus" v-bind:key="submenu.id" link v-bind:to="{ name: submenu.linkTo }" v-bind:disabled="submenu.disabled" v-show="true">
-            <v-list-item-icon>
-              <v-icon>{{ submenu.icon }}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>{{ submenu.title }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-group>
-      </div>
-      <div v-else>
-        <v-list-item link v-bind:to="{ name: item.linkTo }" v-bind:disabled="item.disabled">
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </div>
-    </v-list>
--->
 </v-navigation-drawer>
 </template>
 
@@ -86,8 +53,8 @@ export default {
         { id: 3, title: "アクセス情報",   icon: "mdi-folder", linkTo: "",             disabled: true, group: false, },
         { id: 4, title: "アカウント管理", icon: "mdi-folder", linkTo: "enduser.list", disabled: false, group: true, 
           submenus: [
-            { id: 401, title: "アカウント一覧", icon: "mdi-folder", linkTo: "enduser.list", disabled: false, },
-            { id: 402, title: "アカウント登録", icon: "mdi-folder", linkTo: "enduser.register", disabled: false, },
+            { id: 401, title: "アカウント一覧", linkTo: "enduser.list", disabled: false, },
+            { id: 402, title: "アカウント登録", linkTo: "enduser.register", disabled: false, },
           ],
         },
         { id: 5, title: "契約情報",       icon: "mdi-folder", linkTo: "",             disabled: true, group: false, },

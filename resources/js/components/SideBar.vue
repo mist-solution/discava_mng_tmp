fa45292855f43a644e41922ecf0ec6b0cb2fe4a1
taka_saito <taka_saito@mistnet.co.jp>
2022-11-16 17:07:55 +0900

    不要なコード削除＆アイコン設定

--- resources/js/components/SideBar.vue
+++ resources/js/components/SideBar.vue
@@ -48,18 +48,18 @@ export default {
     return {
       drawer: true,
       items: [
-        { id: 1, title: "投稿管理",       icon: "mdi-folder", linkTo: "news.list",   disabled: false, group: false, },
-        { id: 2, title: "ギャラリー管理", icon: "mdi-folder", linkTo: "",             disabled: true, group: false, },
-        { id: 3, title: "アクセス情報",   icon: "mdi-folder", linkTo: "",             disabled: true, group: false, },
-        { id: 4, title: "アカウント管理", icon: "mdi-folder", linkTo: "enduser.list", disabled: false, group: true, 
+        { id: 1, title: "投稿管理", icon: "mdi-bullhorn-variant-outline ", linkTo: "news.list", disabled: false, group: false, },
+        { id: 2, title: "ギャラリー管理", icon: "mdi-image-outline ", linkTo: "", disabled: true, group: false, },
+        { id: 3, title: "アクセス情報", icon: "mdi-chart-bar ", linkTo: "", disabled: true, group: false, },
+        { id: 4, title: "アカウント管理", icon: "mdi-account", linkTo: "enduser.list", disabled: false, group: true, 
           submenus: [
             { id: 401, title: "アカウント一覧", linkTo: "enduser.list", disabled: false, },
             { id: 402, title: "アカウント登録", linkTo: "enduser.register", disabled: false, },
           ],
         },
-        { id: 5, title: "契約情報",       icon: "mdi-folder", linkTo: "",             disabled: true, group: false, },
-        { id: 6, title: "サイト更新依頼", icon: "mdi-folder", linkTo: "",             disabled: true, group: false, },
-        { id: 7, title: "WEBサイトへ",    icon: "mdi-folder", linkTo: "",             disabled: true, group: false, },
+        { id: 5, title: "契約情報", icon: "mdi-file-document-edit-outline ", linkTo: "", disabled: true, group: false, },
+        { id: 6, title: "サイト更新依頼", icon: "mdi-refresh ", linkTo: "", disabled: true, group: false, },
+        { id: 7, title: "WEBサイトへ", icon: "mdi-tab ", linkTo: "", disabled: true, group: false, },
       ],
       right: null,
     };

96526906dbfe86e95fd5018e9690357686a391df
taka_saito <taka_saito@mistnet.co.jp>
2022-11-21 13:13:08 +0900

    Facades

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,7 @@ use App\Models\ShopUser;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {

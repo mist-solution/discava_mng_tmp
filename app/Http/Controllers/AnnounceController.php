f0e5a39652e4b1dc0504b6e17b15ab3c449dd31a
taka_saito <taka_saito@mistnet.co.jp>
2022-11-21 13:15:32 +0900

    facades

--- app/Http/Controllers/AnnounceController.php
+++ app/Http/Controllers/AnnounceController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Announce;
 use DateTime;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
 
 class AnnounceController extends Controller
 {

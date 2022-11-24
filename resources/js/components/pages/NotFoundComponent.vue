29c410da854d195e5c502abcaee21d70c6944a1a
taka_saito <taka_saito@mistnet.co.jp>
2022-11-25 00:35:35 +0900

    404notfound

--- resources/js/components/pages/NotFoundComponent.vue
+++ resources/js/components/pages/NotFoundComponent.vue
@@ -1,25 +1,22 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">404 Page Not Found</div>
-                    <div class="card-body">ページが存在しません。</div>
-                    <router-link v-bind:to="{ name: 'enduser.list' }">
-                        <button class="btn btn-success ml-1">
-                            戻る
-                        </button>
-                    </router-link>
-                </div>
-            </div>
-        </div>
-    </div>
+  <v-card>
+    <v-card-title class="ml-2" width="80%">
+      <h3 class="h4">ページが存在しません。</h3>
+      <div class="col-md-8">
+        <router-link v-bind:to="{ name: 'enduser.list' }">
+          <button class="btn btn-success ml-1">
+            戻る
+          </button>
+        </router-link>
+      </div>
+    </v-card-title>
+  </v-card>
 </template>
 
 <script>
 export default {
-    mounted() {
-        console.log("example mounted.");
-    },
+  mounted() {
+    console.log("example mounted.");
+  },
 };
 </script>
--- resources/js/router.js
+++ resources/js/router.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const Home = () => import('./components/Home')
-const TaskListComponent = () => import('./components/TaskListComponent')
-const TaskShowComponent = () => import('./components/TaskShowComponent')
-const TaskCreateComponent = () => import('./components/TaskCreateComponent')
-const TaskEditComponent = () => import('./components/TaskEditComponent')
+//const TaskListComponent = () => import('./components/TaskListComponent')
+//const TaskShowComponent = () => import('./components/TaskShowComponent')
+//const TaskCreateComponent = () => import('./components/TaskCreateComponent')
+//const TaskEditComponent = () => import('./components/TaskEditComponent')
 const QuillEditorComponent = () => import('./components/QuillEditor')
 const NewsListComponent = () => import('./components/pages/news_list/NewsListPageComponent')
 const AnnounceRegisterComponent = () => import ('./components/pages/announce/AnnounceRegisterComponent')
@@ -33,7 +33,6 @@ const routes = [
         name: 'news.list',
         component: NewsListComponent
     },
-
     // お知らせ詳細・削除画面
     {
         path: '/news/:announceId',
@@ -41,7 +40,6 @@ const routes = [
         component: NewsDetailComponent,
         props: true
     },
-
     // お知らせ承認画面
     {
         path: '/news/:announceId/approvalProcess',
@@ -49,37 +47,32 @@ const routes = [
         component: NewsApprovalConfirmComponent,
         props: true
     },
-
-    // 一覧画面
-    {
-        path: '/tasks',
-        name: 'task.list',
-        component: TaskListComponent
-    },
-
-    // 詳細画面
-    {
-        path: '/tasks/:taskId',
-        name: 'task.show',
-        component: TaskShowComponent,
-        props: true
-    },
-
-    // 登録画面
-    {
-        path: '/tasks/create',
-        name: 'task.create',
-        component: TaskCreateComponent
-    },
-
-    // 編集画面
-    {
-        path: '/tasks/:taskId/edit',
-        name: 'task.edit',
-        component: TaskEditComponent,
-        props: true
-    },
-
+//    // 一覧画面
+//    {
+//        path: '/tasks',
+//        name: 'task.list',
+//        component: TaskListComponent
+//    },
+//    // 詳細画面
+//    {
+//        path: '/tasks/:taskId',
+//        name: 'task.show',
+//        component: TaskShowComponent,
+//        props: true
+//    },
+//    // 登録画面
+//    {
+//        path: '/tasks/create',
+//        name: 'task.create',
+//        component: TaskCreateComponent
+//    },
+//    // 編集画面
+//    {
+//        path: '/tasks/:taskId/edit',
+//        name: 'task.edit',
+//        component: TaskEditComponent,
+//        props: true
+//    },
     // QuillEditor
     {
         path: '/quilleditor',

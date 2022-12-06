8170dbeb306288fa0980ed8d52ca840efa064793
taka_saito <taka_saito@mistnet.co.jp>
2022-12-06 09:38:38 +0900

    shop change

--- app/Http/Controllers/API/SelectShopController.php
+++ app/Http/Controllers/API/SelectShopController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\ShopUser;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
 
 class SelectShopController extends Controller
@@ -18,15 +19,21 @@ class SelectShopController extends Controller
     {
         // 選択された店舗ID
         $shop_id = $request->input('shop_id');
+        Log::debug('request auth_id:' . Auth::id() . ' shop_id:' . $shop_id);
 
         // 店舗ユーザを取得
-        $shopuser = ShopUser::where('user_id', Auth::id())
+        $shopuser = ShopUser::with('shop')
+            ->where('user_id', Auth::id())
             ->where('shop_id', $shop_id)
+            ->where('del_flg', '0')
             ->first();
+        Log::info('first auth_id:' . Auth::id() . ' shop_id:' . $shop_id);
 
         // 店舗ID、権限を設定
         $request->session()->put('shop_id', $shopuser->shop_id);
         $request->session()->put('authority_id', $shopuser->authority_id);
+
+        Log::info('店舗切り替え user_id:' . Auth::id() . 'shop_id:' . $shopuser->shop_id . ' shop_name' . $shopuser->shop_name);
     }
 
 }

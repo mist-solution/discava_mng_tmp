e8cb689ff8fd39571001476f29214b8262cb313f
taka_saito <taka_saito@mistnet.co.jp>
2022-12-08 22:24:00 +0900

    route modify

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -27,5 +27,5 @@ class ResetPasswordController extends Controller
      * @var string
      */
 //    protected $redirectTo = RouteServiceProvider::HOME;
-    protected $redirectTo = '/auth/complete';
+    protected $redirectTo = '/password/complete';
 }

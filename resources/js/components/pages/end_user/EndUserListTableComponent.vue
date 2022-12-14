7366262c596ba1ca4814de0009665c39a6d4cbda
taka_saito <taka_saito@mistnet.co.jp>
2022-12-14 22:21:58 +0900

    enduser mdi edit

--- resources/js/components/pages/end_user/EndUserListTableComponent.vue
+++ resources/js/components/pages/end_user/EndUserListTableComponent.vue
@@ -15,16 +15,17 @@
             {{ item.title }}
           </router-link>
         </template>
+-->
         <template #item-created_at="item">
           {{ timestampFormat(item.created_at) }}
         </template>
         <template #item-updated_at="item">
           {{ timestampFormat(item.updated_at) }}
         </template>
-        <template #item-approval_status="item">
-          {{ approvalStatusFormat(item.approval_status) }}
+        <template #item-button="item">
+          <v-icon class="green-icon mr-3" @click="edit(item)">mdi-square-edit-outline</v-icon>
+          <v-icon class="green-icon" @click="edit(item)">mdi-trash-can</v-icon>
         </template>
--->
       </EasyDataTable>
 <!--
         <template #loading>
@@ -51,7 +52,7 @@
 <script>
 import NewsListTablePagination from "../news_list/NewsListTablePagination.vue";
 import BackToTopComponent from "../../BackToTopComponent.vue";
-import { mergeProps } from "vue";
+import { inject, mergeProps } from "vue";
 import NewsDeleteConfirmModalComponent from "../news_list/NewsDeleteConfirmModalComponent.vue";
 
 export default {
@@ -69,7 +70,7 @@ export default {
         { text: '登録日時', value: 'created_at' },
         { text: '更新日時', value: 'updated_at' },
         {
-          text: '',
+          text: '編集/削除',
           sortable: false,
           value: 'button',
         },
@@ -122,15 +123,25 @@ export default {
     },
 
     timestampFormat(timestamp) {
-      const date = new Date(timestamp);
-      return (
-        date.getFullYear().toString() +
-        "-" +
-        (date.getMonth() + 1).toString().padStart(2, "0") +
-        "-" +
-        date.getDate().toString().padStart(2, "0")
-      );
+      const dayjs = inject("dayjs");
+      return dayjs(timestamp).format("YYYY/MM/DD HH:mm:ss");
+//      const date = new Date(timestamp);
+//      return (
+//        date.getFullYear().toString() +
+//        "-" +
+//        (date.getMonth() + 1).toString().padStart(2, "0") +
+//        "-" +
+//        date.getDate().toString().padStart(2, "0")
+//      );
     },
+
+    edit(user) {
+      this.$router.push({
+        name: 'enduser.update',
+        params: {user_id: user['id']}
+      });
+    },
+
   },
   mounted() {
     this.getNewsList();

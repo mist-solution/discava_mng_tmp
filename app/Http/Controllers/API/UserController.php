467dad344efb43e64e1fc162ee1c7b256825ae5f
taka_saito <taka_saito@mistnet.co.jp>
2022-12-14 22:05:36 +0900

    enduser list EasyDataTable

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -39,7 +39,7 @@ class UserController extends Controller
         }
         $response['users'] = $userArrays;
         $response['message'] = 'success';
-        return new JsonResponse($response);
+        return $response;
     }
 
     /**
--- resources/js/components/pages/end_user/EndUserListTableComponent.vue
+++ resources/js/components/pages/end_user/EndUserListTableComponent.vue
@@ -1,13 +1,32 @@
 <template>
+  <back-to-top-component />
 
   <v-row>
     <v-col>
       <EasyDataTable
         v-model:items-selected="selected"
         :headers="headers"
-        :items="news"
+        :items="users"
         dense
       >
+<!--
+        <template #item-title="item">
+          <router-link :to="{ name: 'announce.edit', params: { announceId: item.id } }">
+            {{ item.title }}
+          </router-link>
+        </template>
+        <template #item-created_at="item">
+          {{ timestampFormat(item.created_at) }}
+        </template>
+        <template #item-updated_at="item">
+          {{ timestampFormat(item.updated_at) }}
+        </template>
+        <template #item-approval_status="item">
+          {{ approvalStatusFormat(item.approval_status) }}
+        </template>
+-->
+      </EasyDataTable>
+<!--
         <template #loading>
           <v-progress-linear
             indeterminate
@@ -15,186 +34,107 @@
             color="primary"
           />
         </template>
-        <template #item-name="item">
-          {{ timestampFormat(item.name) }}
-        </template>
-<!--
-        <template #item-email="item">
-          {{ timestampFormat(item.email) }}
-        </template>
 -->
-        <template #item-created_at="item">
-          {{ timestampFormat(item.created_at) }}
-        </template>
-        <template #item-updated_at="item">
-          {{ timestampFormat(item.updated_at) }}
-        </template>
-
-        <template #item-id="item">
-          <router-link :to="{ name: 'enduser.update', params: { userId: item.id } }">
-            <v-icon class="green-icon mr-3" @click="edit(item)">mdi-square-edit-outline</v-icon>
-          </router-link>
-          <v-icon class="green-icon"
-            @click.stop="(displayEndUserDeleteConfirm = true),
-              setDeleteUserId(item.id)">
-            mdi-trash-can
-          </v-icon>
-        </template>
-
-      </EasyDataTable>
     </v-col>
   </v-row>
 
-<!--
-  <v-table class="accounttable" fixed-header>
-    <thead  class="displaypc">
-      <tr>
-        <th class="text-left"></th>
-        <th class="text-left">ユーザID</th>
-        <th class="text-left">メールアドレス</th>
-        <th class="text-left">登録日時</th>
-        <th class="text-left">更新日時</th>
-        <th class="text-left">編集/削除</th>
-      </tr>
-    </thead>
-    <tbody class="displaypc">
-      <tr v-for="item in users" :key="item.id">
-        <td>
-          <input type="checkbox" :value="item.id" v-model="selected">
-        </td>
-        <td>{{ item.name }}</td>
-        <td>{{ item.email }}</td>
-        <td>{{ timestampFormat(item.created_at) }}</td>
-        <td>{{ timestampFormat(item.updated_at) }}</td>
-        <td class="d-flex align-center">
-          <v-icon class="green-icon mr-3" @click="edit(item)">mdi-square-edit-outline</v-icon>
-          <v-icon class="green-icon" @click="edit(item)">mdi-trash-can</v-icon>
-        </td>
-      </tr>
-    </tbody>
-
-    <div class="displaysp">
-      <tbody>
-        <tr v-for="item in users" :key="item.id">
-          <div @click="toggleAccordion()"><input type="checkbox" :value="item.id" v-model="selected">{{ item.name }}</div>
-            <div v-if="isOpened">
-                <tr>
-                  <td>ID</td>
-                  <td>{{ item.name }}</td>
-                </tr>
-                <tr>
-                  <td>メール</td>
-                  <td>{{ item.email }}</td>
-                </tr>
-                <tr>
-                  <td>登録日</td>
-                  <td>{{ timestampFormat(item.created_at) }}</td>
-                </tr>
-                <tr>
-                  <td>更新日</td>
-                  <td>{{ timestampFormat(item.updated_at) }}</td>
-                </tr>
-                <tr>
-                  <td class="d-flex align-center">
-                    <v-icon class="green-icon mr-3" @click="edit(item)">mdi-square-edit-outline</v-icon>
-                    <v-icon class="green-icon" @click="edit(item)">mdi-trash-can</v-icon>
-                  </td>
-                </tr>
-            </div>
-        </tr>
-      </tbody>
-    </div>
-  </v-table>
--->
   <!-- 削除モーダル -->
-  <end-user-delete-confirm-modal-component
-    :display="displayEndUserDeleteConfirm"
+<!--
+  <news-delete-confirm-modal-component
+    :display="displayNewsDeleteConfirm"
     :closeAction="closeAction"
-    :deleteUser="deleteUser"
+    :deleteAnnounce="deleteAnnounce"
   />
+-->
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
-import { inject } from "vue";
-import EndUserDeleteConfirmModalComponent from "./EndUserDeleteConfirmModalComponent.vue";
+import NewsListTablePagination from "../news_list/NewsListTablePagination.vue";
+import BackToTopComponent from "../../BackToTopComponent.vue";
+import { mergeProps } from "vue";
+import NewsDeleteConfirmModalComponent from "../news_list/NewsDeleteConfirmModalComponent.vue";
 
 export default {
   components: {
-    EndUserDeleteConfirmModalComponent,
+    NewsListTablePagination,
+    BackToTopComponent,
+    NewsDeleteConfirmModalComponent,
   },
   data() {
     return {
       selected: [],
-      isOpened: false,
-      displayEndUserDeleteConfirm: false,
-      
+
       headers: [
+        { text: 'ユーザ名', value: 'name' },
+        { text: '登録日時', value: 'created_at' },
+        { text: '更新日時', value: 'updated_at' },
         {
           text: '',
           sortable: false,
+          value: 'button',
         },
-        { text: 'ユーザ名', value: 'name' },
-//        { text: 'メールアドレス', value: 'email' },
-        { text: '登録日時', value: 'created_at' },
-        { text: '更新日時', value: 'updated_at' },
-        { text: '編集/削除', value: 'id' },
       ],
+      users: [],
+      displayNewsDeleteConfirm: false,
+      menuDeleteAnnounce: [],
+      select: [],
     };
   },
+  computed: {
+  },
+  watch: {
+  },
   methods: {
-    ...mapActions('enduser', ['fetchUsers']),
-    ...mapActions('customer', ['fetchCustomers']),
-    ...mapActions("snackbar", ["openSuccess", "openWarning", "openError", "closeSnackbar"]),
-    timestampFormat(timestamp) {
-      const dayjs = inject("dayjs");
-      return dayjs(timestamp).format("YYYY/MM/DD HH:mm:ss");
+    mergeProps,
+    closeAction() {
+      this.displayNewsDeleteConfirm = false;
+      window.location.reload();
     },
-    edit(user) {
-      this.$router.push({
-        name: 'enduser.update',
-        params: {user_id: user['id']}
-      });
-    },
-    deleteAll() {
-      if (this.selected.length === 0) {
-        this.openError('対象が選択されていません');
-        return;
-      }
-      this.$axios.post('/api/enduser/delete', this.selected)
-        .then(response => {
-            this.openSuccess('削除しました');
-            this.fetchUsers();
+
+    getNewsList() {
+//      this.loading = true;
+      axios
+        .get("/api/enduser", {
         })
-        .catch(error => {
-            console.log(error);
+        .then((res) => {
+          this.users = res.data.users;
+//          this.$store.dispatch("news/setTotalCount", res.data.count);
+//          this.loading = false;
         });
     },
+
+    // 選択した記事をstoreに設定
+    setSelectItems() {
+      this.$store.dispatch("news/setDisplayCheckedItems", this.select);
+    },
+
     // 削除確認ダイアログに渡せるため、IDをstoreに設定
-    setDeleteUserId(id) {
-      let userId = id;
-      this.$store.dispatch("enduser/setDeleteUserId", userId);
+    setDeleteAnnounceId(id) {
+      let announceId = id;
+      this.$store.dispatch("news/setDeleteNewsId", announceId);
     },
+
     // 削除処理
-    deleteUser(userId) {
-      console.log(`ID:${userId} が削除しました。`);
-      axios.delete("/api/enduser/" + userId).then((res) => {});
+    deleteAnnounce(announceId) {
+      console.log(`ID:${announceId} が削除しました。`);
+      axios.delete("/api/announce/" + announceId).then((res) => {});
       window.location.reload();
     },
-    closeAction() {
-      this.displayEndUserDeleteConfirm = false;
-      window.location.reload();
+
+    timestampFormat(timestamp) {
+      const date = new Date(timestamp);
+      return (
+        date.getFullYear().toString() +
+        "-" +
+        (date.getMonth() + 1).toString().padStart(2, "0") +
+        "-" +
+        date.getDate().toString().padStart(2, "0")
+      );
     },
-    toggleAccordion() {
-      this.isOpened = !this.isOpened;
-    }
   },
   mounted() {
-    this.fetchUsers();
-    this.fetchCustomers();
-  },
-  computed: {
-    ...mapState("enduser", ["users"]),
+    this.getNewsList();
+    this.setSelectItems();
   },
 };
 </script>
@@ -209,10 +149,10 @@ export default {
 }
 
 .accounttable td {
-	background: #F7F7F7;
+        background: #F7F7F7;
 }
 .accounttable tr:nth-child(odd) td {
-	background: #fff;
+        background: #fff;
 }
 
 /* vue3-data-tableで罫線固定しているためimportantで指定 */
@@ -240,7 +180,7 @@ export default {
   }
 
   .accounttable td {
-	background: #FFF;
+        background: #FFF;
   }
 
   .displaypc {
@@ -252,4 +192,4 @@ export default {
   }
 }
 
-  </style>
+</style>

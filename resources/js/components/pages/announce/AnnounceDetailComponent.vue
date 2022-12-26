7a0988e72a21ddc5970712c61be7bab310697d17
Seikeirinn <hl_sheng@mistnet.co.jp>
2022-12-26 16:31:28 +0800

    お知らせ詳細表示

---
+++ resources/js/components/pages/announce/AnnounceDetailComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="row justify-body-center">
+      <v-card>
+        <v-row>
+          <v-col class="mt-3">
+            <v-card-title>タイトル：{{ this.announce.title }}</v-card-title>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-card-text>本文：
+              <p v-html="announce.body">
+              </p>
+            </v-card-text>
+          </v-col>
+        </v-row>
+      </v-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    announceId: String,
+  },
+  data: function () {
+    return {
+      announce: {},
+    };
+  },
+  methods: {
+    getannounce() {
+      this.$axios.post("/webapi/announce/detail/" + this.announceId).then((res) => {
+        this.announce = res.data;
+        console.log(this.announce)
+      });
+    },
+  },
+  mounted() {
+    this.getannounce();
+  },
+};
+</script>
--- resources/js/router.js
+++ resources/js/router.js
@@ -7,8 +7,9 @@ const Home = () => import('./components/Home')
 //const TaskEditComponent = () => import('./components/TaskEditComponent')
 const QuillEditorComponent = () => import('./components/QuillEditor')
 const NewsListComponent = () => import('./components/pages/news_list/NewsListPageComponent')
-const AnnounceRegisterComponent = () => import ('./components/pages/announce/AnnounceRegisterComponent')
-const AnnounceEditComponent = () => import ('./components/pages/announce/AnnounceEditComponent')
+const AnnounceRegisterComponent = () => import('./components/pages/announce/AnnounceRegisterComponent')
+const AnnounceDetailComponent = () => import('./components/pages/announce/AnnounceDetailComponent')
+const AnnounceEditComponent = () => import('./components/pages/announce/AnnounceEditComponent')
 const EndUserRegistComponent = () => import('./components/pages/end_user/EndUserRegistComponent')
 const EndUserUpdateComponent = () => import('./components/pages/end_user/EndUserUpdateComponent')
 const EndUserListComponent = () => import('./components/pages/end_user/EndUserListComponent')
@@ -18,11 +19,11 @@ const NotFoundComponent = () => import('./components/pages/NotFoundComponent')
 
 const routes = [
     // ダッシュボード画面
-//    {
-//        path: '/',
-//        name: 'home',
-//        component: Home
-//    },
+    //    {
+    //        path: '/',
+    //        name: 'home',
+    //        component: Home
+    //    },
     // ※初回リリース時点ではお知らせ一覧に直接遷移します。
     {
         path: '/',
@@ -48,32 +49,32 @@ const routes = [
         component: NewsApprovalConfirmComponent,
         props: true
     },
-//    // 一覧画面
-//    {
-//        path: '/tasks',
-//        name: 'task.list',
-//        component: TaskListComponent
-//    },
-//    // 詳細画面
-//    {
-//        path: '/tasks/:taskId',
-//        name: 'task.show',
-//        component: TaskShowComponent,
-//        props: true
-//    },
-//    // 登録画面
-//    {
-//        path: '/tasks/create',
-//        name: 'task.create',
-//        component: TaskCreateComponent
-//    },
-//    // 編集画面
-//    {
-//        path: '/tasks/:taskId/edit',
-//        name: 'task.edit',
-//        component: TaskEditComponent,
-//        props: true
-//    },
+    //    // 一覧画面
+    //    {
+    //        path: '/tasks',
+    //        name: 'task.list',
+    //        component: TaskListComponent
+    //    },
+    //    // 詳細画面
+    //    {
+    //        path: '/tasks/:taskId',
+    //        name: 'task.show',
+    //        component: TaskShowComponent,
+    //        props: true
+    //    },
+    //    // 登録画面
+    //    {
+    //        path: '/tasks/create',
+    //        name: 'task.create',
+    //        component: TaskCreateComponent
+    //    },
+    //    // 編集画面
+    //    {
+    //        path: '/tasks/:taskId/edit',
+    //        name: 'task.edit',
+    //        component: TaskEditComponent,
+    //        props: true
+    //    },
     // QuillEditor
     {
         path: '/quilleditor',
@@ -86,6 +87,13 @@ const routes = [
         path: '/announce/register',
         name: 'announce.register',
         component: AnnounceRegisterComponent,
+    },
+    // お知らせ詳細
+    {
+        path: '/announce/detail/:announceId',
+        name: 'announce.detail',
+        component: AnnounceDetailComponent,
+        props: true
     },
     // お知らせ編集
     {

6b3f51d0f38505ae6c5807c4ab07b93536ab59f1
yanoaya <yano_a@mistnet.co.jp>
2023-01-06 11:29:14 +0900

    スナックバーのデザイン変更

--- resources/js/components/Snackbar.vue
+++ resources/js/components/Snackbar.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <v-snackbar v-model="$store.state.snackbar.snackbar" :timeout="timeout" :color="$store.state.snackbar.color" :top="true">
+    <v-snackbar
+        v-model="$store.state.snackbar.snackbar"
+        :timeout="timeout"
+        :top="true"
+      >
       {{ $store.state.snackbar.text }}
     </v-snackbar>
   </div>
@@ -13,3 +17,19 @@
     }),
   };
 </script>
+<style>
+  /* スナックバーの横幅を広げる、 */
+  .v-snackbar__content {
+    width: 100vw;
+    bottom: 50vh !important;
+    text-align: center;
+    background-color: #69A5AF;
+    color: #fff;
+  }
+  .v-snackbar__wrapper {
+    bottom: 50vh !important;
+  }
+  .v-snackbar {
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+</style>

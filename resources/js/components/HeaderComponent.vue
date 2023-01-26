cc11f4e84e198f5a356d8356e9684aa2be142674
yanoaya <yano_a@mistnet.co.jp>
2023-01-26 22:38:52 +0900

    ser.82

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -7,25 +7,61 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" @click="setting"><v-icon>mdi-cog</v-icon></a>
+                <a
+                  class="nav-link active header-icon"
+                  aria-current="page"
+                  @click="(displayDevelopMentModal = true)"
+                >
+                  <v-icon>mdi-cog</v-icon>
+                </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" @click="notice"><v-icon>mdi-bell</v-icon></a>
+                <a
+                  class="nav-link active header-icon"
+                  aria-current="page"
+                  @click="(displayDevelopMentModal = true)"
+                >
+                  <v-icon>mdi-bell</v-icon>
+                </a>
               </li>
             </ul>
           </div>
         </div>
       </nav>
     </div>
+    <development-modal-component
+      :modelValue="displayDevelopMentModal"
+      @update:modelValue="displayDevelopMentModal = $event"
+      :closeAction="closeReturn"
+    />
   </div>
 </template>
 
 <script>
+import DevelopmentModalComponent from "../components/modals/DevelopmentModalComponent.vue"
+
 export default {
+  components: {
+    DevelopmentModalComponent,
+  },
+  data() {
+    return {
+      displayDevelopMentModal: false,
+    }
+  },
   methods: {
+    closeReturn() {
+      this.displayDevelopMentModal = false;
+    },
   },
   mounted() {
     console.log("header mounted.");
   },
 };
 </script>
+
+<style scoped>
+.header-icon {
+  cursor: pointer;
+}
+</style>
---
+++ resources/js/components/modals/DevelopmentModalComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-dialog v-show="display" persistent>
+      <v-card class="p-3">
+        <!-- モーダルタイトル -->
+        <v-card-title class="text-center modal-title">
+          <v-icon color="#707070" x-large>mdi-vlc</v-icon>
+        </v-card-title>
+        <!--  モーダル説明文 -->
+        <v-card-text class="text-center mb-3">
+          ただいま開発中です。<br />
+          完成までもう少々お待ちください。
+        </v-card-text>
+        <!-- 操作 -->
+        <v-card-actions class="justify-center">
+            <!-- 閉じるボタン -->
+            <v-btn @click="closeAction()" class="gray-btn mx-2">閉じる</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </template>
+  
+  <script>
+  export default {
+    props: ["closeAction"],
+    data() { return {}; },
+    methods: {},
+    mounted() {},
+  };
+  </script>

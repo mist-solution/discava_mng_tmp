96bf1b5a0a59c05744eed3d5030b61c3be01cf1c
yanoaya <yano_a@mistnet.co.jp>
2023-02-24 10:37:03 +0900

    404ページのデザイン微修正

--- resources/js/components/pages/NotFoundComponent.vue
+++ resources/js/components/pages/NotFoundComponent.vue
@@ -1,16 +1,18 @@
 <template>
-  <v-card class="error_message">
-    <v-card-title class="ml-2" width="80%">
-      <h3 class="h4">ページが存在しません。</h3>
-      <div class="col-md-8">
-        <router-link v-bind:to="{ name: 'announce.list' }">
-          <button class="btn btn-success ml-1">
-            戻る
-          </button>
-        </router-link>
-      </div>
-    </v-card-title>
-  </v-card>
+    <v-container>
+      <v-card class="error_message mx-5">
+        <v-card-title class="ml-2 text-center" width="80%">
+        <h3 class="h4 mt-5">ご指定のページは存在しません。</h3>
+        <div class="col-md-12">
+          <router-link v-bind:to="{ name: 'announce.list' }">
+            <button class="btn my-5 green-btn">
+              投稿管理に戻る
+            </button>
+          </router-link>
+        </div>
+      </v-card-title>
+    </v-card>
+  </v-container>
 </template>
 
 <style src="../pages/css/common.css"></style>

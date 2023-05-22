dfea23a04c5184006e3f40df68f6f3dabd4416a5
Seikeirinn <hl_sheng@mistnet.co.jp>
2023-05-22 15:33:45 +0900

    ライブラリ、フォルダ検索の提示文言修正

--- resources/js/store/gallery.js
+++ resources/js/store/gallery.js
@@ -1,20 +1,36 @@
 const state = {
-    galleryHintMessages: null,
+    galleryHintMessagesInLibrary: null,
+    galleryHintMessagesInFolder: null,
 }
 const getters = {
 
 }
 
 const mutations = {
-    setGalleryHintMessages(state, messages) {
-        state.galleryHintMessages = messages;
+    setGalleryHintMessagesLibrary(state, messages) {
+        state.galleryHintMessagesInLibrary = messages;
+        console.log("galleryHintMessagesInLibrary 1")
+        console.log(messages)
     },
-}
+    setGalleryHintMessagesFolder(state, messages) {
+        state.galleryHintMessagesInFolder = messages;
+        console.log("galleryHintMessagesInFolder 1")
+        console.log(messages)
+    },
+};
+
 const actions = {
-    setGalleryHintMessages(context, messages) {
-        context.commit('setGalleryHintMessages', messages)
+    setGalleryHintMessagesLibrary(context, messages) {
+        context.commit('setGalleryHintMessagesLibrary', messages);
+        console.log("setGalleryHintMessagesLibrary 2")
+        console.log(messages)
     },
-}
+    setGalleryHintMessagesFolder(context, messages) {
+        context.commit('setGalleryHintMessagesFolder', messages);
+        console.log("setGalleryHintMessagesFolder 2")
+        console.log(messages)
+    },
+};
 
 export default {
     namespaced: true, // 名前空間を有効にする
